2bcfd8bbcd1d38540fb164eb1b3d68c3b8f2130c
Jessica Kelly <jessie@familyilove.com>
2020-07-10 18:34:22 -0600

    Use minified/compressed css version.

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from "react-redux";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./helpers/setAuthToken";
 import { setCurrentUser, logoutUser } from "./redux/actions/authActions";
-import "./sass/index.scss";
+import "./sass/index.min.scss";
 import "materialize-css/dist/css/materialize.min.css";
 import store from "./redux/store/index";
 import * as serviceWorker from "./serviceWorker";
