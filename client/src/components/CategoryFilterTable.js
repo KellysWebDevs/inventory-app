cf1cc8d2793b7328563132633feb79d52cfc9e1d
Adam Kelly <adamkelly@Adams-MBP.lan>
2020-07-13 16:53:27 -0600

    Add inline editing for item amount.

--- client/src/components/CategoryFilterTable.js
+++ client/src/components/CategoryFilterTable.js
@@ -24,7 +24,7 @@ class CategoryFilterTable extends React.Component {
                 <tr>
                   <th className="pl-1">Item</th>
                   <th className="center-align">Amount</th>
-                  <th className="barcode-header center-align">Bar Code</th>
+                  <th className="barcode-header center-align">Barcode</th>
                   <th className="remove-header center-align">Remove</th>
                 </tr>
               </thead>
--- client/src/components/TableItem.js
+++ client/src/components/TableItem.js
@@ -50,8 +50,6 @@ class TableItem extends React.Component {
   };
 
   render() {
-    const { item } = this.props;
-
     return (
       <tr>
         <td className="pl-1">
@@ -65,7 +63,18 @@ class TableItem extends React.Component {
             onBlur={this.handleBlur}
           />
         </td>
-        <td className="center-align">{item.amount}</td>
+        <td className="center-align">
+          <input
+            style={{ borderBottom: "none" }}
+            className="center-align"
+            name="item_amount"
+            type="number"
+            value={this.state.item_amount}
+            onChange={this.handleChange}
+            onKeyDown={this.handleKeyDown}
+            onBlur={this.handleBlur}
+          />
+        </td>
         <td className="barcode-cell center-align">
           <InlineIcon icon={barcodeScan} width="1.5em" height="1.5em" />
         </td>
